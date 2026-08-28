8e06a874212968abbc86359b6979dcfcc30d4499
JohannSierra <redbird1.js@hotmail.com>
2026-08-28 12:00:24 -0600

    Refactor: Restructure project layout to official Vintage Story mod template with ModDB packager

---
+++ scratch_template/WaypointTeleportModModSystem.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+[assembly: ModInfo("WaypointTeleportMod", "WaypointTeleportMod",
+                    Authors = new string[] { "Unknown" },
+                    Description = "This is a sample mod",
+                    Version = "1.0.0")]
+namespace WaypointTeleportMod
+{
+    public class WaypointTeleportModModSystem : ModSystem
+    {
+    }
+}
---
+++ scratch_template2/WaypointTeleportMod/WaypointTeleportModModSystem.cs
@@ -0,0 +1,7 @@
+﻿using Vintagestory.API.Common;
+
+namespace WaypointTeleportMod;
+
+public class WaypointTeleportModModSystem : ModSystem
+{
+}
